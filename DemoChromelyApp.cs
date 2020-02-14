2061aac05a1f1430fd5681fc82637c78f9579fd8
Antony Capper <acapper56@gmail.com>
2020-02-14 16:15:05 +0000

    Initial commit

---
+++ DemoChromelyApp.cs
@@ -0,0 +1,14 @@
+using Chromely;
+using Chromely.Core;
+
+
+namespace My_Chromely_App
+{
+	class DemoChromelyApp : ChromelyBasicApp
+	{
+		public override void Configure(IChromelyContainer container)
+		{
+			base.Configure(container);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Chromely.Core;
+using Chromely.Core.Configuration;
+
+namespace My_Chromely_App
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			//IChromelyConfiguration config = DefaultConfiguration.CreateForRuntimePlatform();
+
+			AppBuilder
+			.Create()
+			.UseApp<DemoChromelyApp>()
+			.Build()
+			.Run(args);
+		}
+
+	}
+}
